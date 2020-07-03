26f3aa0d3803d3829b821d12c67ade6f58522d76
Joshua Dowding <joshuadowding@outlook.com>
2020-07-03 15:41:27 +0100

    remove elements from hierarchy using react-dom instead of jquery

--- src/js/renderer/controllers/editor-controller.js
+++ src/js/renderer/controllers/editor-controller.js
@@ -1,4 +1,4 @@
-module.exports = { addItemToHierarchy }
+module.exports = { addItemToHierarchy, clearItemsInHierarchy }
 
 const React = require('react');
 const ReactDOM = require('react-dom');
@@ -7,4 +7,8 @@ const { Item } = require('../components/item');
 
 function addItemToHierarchy(data, parent) {
   ReactDOM.render(<Item items={data} />, parent);
+}
+
+function clearItemsInHierarchy(parent) {
+  ReactDOM.unmountComponentAtNode(parent);
 }
